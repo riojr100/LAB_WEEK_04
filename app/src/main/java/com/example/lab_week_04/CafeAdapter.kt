0b07c8efd3d4dd0cf34f57cac5c87fdff46ac7ee
riojr100 <riojr100@gmail.com>
2024-09-26 18:46:03 +0700

    kode kapal lawd 🚢⛵
    assignment selesai  yipeee 🎉🎉
    
    Co-Authored-By: kenhanbit <147574395+kenhanbit@users.noreply.github.com>

--- app/src/main/java/com/example/lab_week_04/CafeAdapter.kt
+++ app/src/main/java/com/example/lab_week_04/CafeAdapter.kt
@@ -1,5 +1,6 @@
 package com.example.lab_week_04
 
+import android.content.Context
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.lifecycle.Lifecycle
@@ -10,13 +11,23 @@ val TABS_FIXED = listOf(
     R.string.janjijiwa_title,
     R.string.kopikenangan_title,
 )
-class CafeAdapter(fragmentManager: FragmentManager, lifecycle: Lifecycle) :
+
+val TABS_CONTENT_PLS_HELP_MY_LIFE = listOf(
+    R.string.starbucks_desc,
+    R.string.janjijiwa_desc,
+    R.string.kopikenangan_desc
+)
+
+class CafeAdapter(private val appcontext: Context,
+                  fragmentManager: FragmentManager,
+                  lifecycle: Lifecycle) :
     FragmentStateAdapter(fragmentManager, lifecycle) {
     override fun getItemCount(): Int {
         return TABS_FIXED.size
     }
     override fun createFragment(position: Int): Fragment
     {
-        return CafeDetailFragment()
+        val desc = appcontext.getString(TABS_CONTENT_PLS_HELP_MY_LIFE[position])
+        return CafeDetailFragment.newInstance(desc)
     }
 }

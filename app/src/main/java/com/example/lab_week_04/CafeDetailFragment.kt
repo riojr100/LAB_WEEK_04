4f1c077dda23922cadc197c86ff7a9d8fdeded3f
riojr100 <riojr100@gmail.com>
2024-09-26 18:47:41 +0700

    rapihin kode
    kenndy
    
    k - kapal lawd
    
    Co-Authored-By: kenhanbit <147574395+kenhanbit@users.noreply.github.com>

--- app/src/main/java/com/example/lab_week_04/CafeDetailFragment.kt
+++ app/src/main/java/com/example/lab_week_04/CafeDetailFragment.kt
@@ -27,7 +27,6 @@ class CafeDetailFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-// Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_cafe_detail, container, false)
     }
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
